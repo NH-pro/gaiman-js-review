6b151bb719c97f2ab802ec5a47347c94a04236eb
Neil Hanson <pro.neilhanson@gmail.com>
2022-05-09 11:35:54 -0500

    added some notes

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('hello');
+
+let myName = 'Neil';
+// END global var 'myName'
+console.log(`my name is ${myName}`);
+// 
+
+
+/*
+let --- a keyword, we are about to delare a variable
+
+myName --- the name of the variable
+
+= --- the assignement operator 
+
+'Neil' --- the value
+*/
+
+
+// Use camelCase!!!!!
+let lastName = 'Hanson';
+
+// Other kinds of casing:
+
+// snake_case
+//let my_last_name = 'Hanson';
+
+// SCREAMING_SNAKE_CASE
+// MY_LAST_NAME = 'Hanson';
+
+// kebob-case
+//let my-last-name = 'Hanson';
